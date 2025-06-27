392acf00f31f6514d4a222aa87d9c4e9200338a8
“Erick” <152939401+bratugues@users.noreply.github.com>
2025-06-27 14:38:09 +0200

    final code 1

--- index.js
+++ index.js
@@ -35,3 +35,46 @@
 // - "o {tipo} atacou usando {ataque}"
 //   ex: mago atacou usando magia
 //   guerreiro atacou usando espada
+
+class Hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar(){
+        console.log(`Nome: ${this.nome}`)
+        console.log(`Idade: ${this.idade}`)
+        console.log(`Tipo: ${this.tipo}`)
+
+        let ataque;
+        if(this.tipo === "mago"){
+            ataque = "magia"
+        } else if (this.tipo === "guerreiro"){
+            ataque = "espada"
+        } else if (this.tipo === "monge"){
+            ataque = "artes marciais"
+        } else if (this.tipo === "ninja"){
+            ataque = "shuriken"
+        } else{
+            console.log ("Escolha um dos tipos disponíveis.")
+        }
+        console.log (`O ${this.tipo} atacou usando ${ataque}.`)
+    }
+}
+
+const mago = new Hero("Ogam", 28, "mago");
+const guerreiro = new Hero("Orierreug", 31, "guerreiro");
+const monge = new Hero("Egnom", 67, "monge");
+const ninja = new Hero("Ajnin", 33, "ninja");
+
+function heroAttack(){
+let herois = [mago, guerreiro, monge, ninja]
+for (let i=0; i<herois.length; i++){
+    herois[i].atacar()
+}
+}
+
+heroAttack()
+
+
